180af7e0eed34075dace719e8664c9157f93b058
j-sabile@gmail.com <jericosabile@gmail.com>
2023-08-23 11:30:45 +0800

    add typing feature and optimize code

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,85 +1,4 @@
-// import { useState } from "react";
-// import NavBar from "../components/NavBar";
-
-// function Chat() {
-//   const [conversations, setConversations] = useState([]);
-//   const userType = localStorage.getItem("userType");
-
-//   return (
-//     <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-//       <NavBar />
-//       <h3>Chat</h3>
-//       <div style={{ display: "flex", flexGrow: "1" }}>
-//         {/* SIDE BAR */}
-//         <div style={{ display: "flex", flexDirection: "column", width: "250px" }}>
-//           {conversations?.map((chat, index) => (
-//             <div key={index} style={{ border: "1px solid black", padding: "0.5rem 1rem", cursor: "pointer" }} onClick={() => handleSelectChat(chat._id)}>
-//               {userType === "Customer" && <div style={{ fontWeight: "500" }}>{chat.shop.name}</div>}
-//               {userType === "Shop" && <div style={{ fontWeight: "500" }}>{`${chat.customer.first_name} ${chat.customer.last_name}`}</div>}
-//               {userType !== chat.last_message?.sender && <div>{chat.last_message?.message}</div>}
-//               {userType === chat.last_message?.sender && <div>{`You: ${chat.last_message?.message}`}</div>}
-//             </div>
-//           ))}
-//         </div>
-//         {selectedChat !== null && (
-//           <div style={{ flex: 1, border: "1px solid black", padding: "0.5rem", display: "flex", flexDirection: "column" }}>
-//             {/* HEADER */}
-//             <div>
-//               <h5>
-//                 {userType === "Shop" && `${selectedChat.customer.first_name} ${selectedChat.customer.last_name}`}
-//                 {userType === "Customer" && selectedChat.shop.name}
-//               </h5>
-//             </div>
-//             {/* MESSAGES */}
-//             <div style={{ display: "flex", flexDirection: "column", gap: "0.20rem", overflowY: "scroll", flexGrow: "1", height: "1px", padding: "1rem 0rem" }}>
-//               {selectedChat.messages.map((chat, index) => (
-//                 <div key={index}>
-//                   {chat?.sender === userType ? (
-//                     <div style={{ display: "flex", justifyContent: "end" }}>
-//                       <div style={{ border: "1px solid black", padding: "0rem 0.75rem", marginLeft: "4rem", borderRadius: "20px" }}>{chat.message}</div>
-//                     </div>
-//                   ) : (
-//                     <div style={{ display: "flex", justifyContent: "start", gap: "0.5rem" }}>
-//                       {displayProfilePicture(index)}
-//                       <div style={{ border: "1px solid black", padding: "0rem 0.75rem", marginRight: "4rem", borderRadius: "20px" }}>{chat.message}</div>
-//                     </div>
-//                   )}
-//                 </div>
-//               ))}
-//               {/* SENDING MESSAGES */}
-//               {selectedChat.sending.length > 0 && (
-//                 <div style={{ display: "flex", flexDirection: "column", gap: "0.20rem", marginTop: "0.5rem" }}>
-//                   {selectedChat.sending.map((message, index) => (
-//                     <div key={index} style={{ display: "flex", justifyContent: "end" }}>
-//                       <div style={{ border: "1px solid", borderColor: "#555555", padding: "0rem 0.75rem", marginLeft: "4rem", color: "#555555", borderRadius: "20px" }}>{message}</div>
-//                     </div>
-//                   ))}
-//                   <div style={{ display: "flex", justifyContent: "end" }}>
-//                     <div style={{ fontSize: "14px", color: "#333333" }}>Sending...</div>
-//                   </div>
-//                 </div>
-//               )}
-//             </div>
-//             {/* TEXT BOX */}
-//             <form style={{ display: "flex" }}>
-//               <input type="text" placeholder="Enter a message" value={message} onChange={(e) => setMessage(e.target.value)} style={{ flex: 1 }} />
-//               <button type="submit" onClick={handleSend}>
-//                 Send
-//               </button>
-//             </form>
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Chat;
-
-////////////////////////////////////////////////////////
-
-import { useRef } from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 import NavBar from "../components/NavBar";
 
@@ -90,9 +9,8 @@ function Chat() {
   const [selectedChat, setSelectedChat] = useState(null);
   const [socket, setSocket] = useState(null);
   const [message, setMessage] = useState("");
-  const [loading, setLoading] = useState(true);
-
-  const s = useRef(null);
+  const [sendTypingInterval, setSendingTypeInterval] = useState(null);
+  const [sendTypingIntervalRunning, setSendTypingIntervalRunning] = useState(false);
   const conversationsRef = useRef(conversations);
 
   useEffect(() => {
@@ -102,94 +20,82 @@ function Chat() {
         else throw new Error(res.status);
       })
       .then((res) => {
-        const request = res.request.map((i) => ({ ...i, sending: [] }));
+        const request = res.request.map((i) => ({ ...i, sending: [], isTyping: false, intervalId: null }));
         setConversations(request);
         conversationsRef.current = request;
       });
 
     const localSocket = io(import.meta.env.VITE_SOCKET_API, { withCredentials: true });
-    localSocket.emit("seen", { conversationId: "123", messageId: "345" });
     setSocket(localSocket);
-
-    localSocket.on("message", ({ doc, conversationId }) => {
-      console.log("Received a message:", doc);
-      const temp = [...conversationsRef.current];
-
-      for (let i = 0; i < temp.length; i++) {
-        if (temp[i]._id === conversationId) {
-          temp[i].messages.push(doc);
-          temp[i].last_message = { message: doc.message, sender: doc.sender, timestamp: doc.timestamp };
-          temp.unshift(temp[i]);
-          temp.splice(i + 1, 1);
-          break;
-        }
-      }
-      setConversations(temp);
-      conversationsRef.current = temp;
-    });
-
-    // message is sent
-    localSocket.on("ack_message", ({ doc, conversationId }) => {
-      console.log("ack_message", doc.message);
-      // const tempMessages = [....current];
-      const tempConversations = [...conversationsRef.current];
-      // console.log("old tempConversations", tempConversations);
-      // console.log("doc:", doc);
-      // setConversations((tempConversations) => {
-      for (let i = 0; i < tempConversations.length; i++) {
-        if (tempConversations[i]._id === conversationId) {
-          const index = tempConversations[i].sending.findIndex((j) => j === doc.message);
-          console.log(index);
-          if (index !== -1) tempConversations[i].sending.splice(index, 1);
-          // console.log("new tempConversations", tempConversations);
-          // console.log("changing conversations");
-          tempConversations[i].messages.push(doc);
-          console.log(tempConversations);
-          conversationsRef.current = tempConversations;
-
-          break; // return tempConversations;
-        }
-      }
-      console.log("ack", tempConversations);
-      setConversations(tempConversations);
-
-      // console.log("ack messages:", tempMessages);
-      // for (let i = 0; i < tempMessages.length; i++) {
-      //   console.log(tempMessages[i], doc.message, tempMessages[i] === doc.message);
-      //   if (tempMessages[i] === doc.message) {
-      //     const temp = tempMessages;
-      //     temp.splice(i, 1);
-      //     console.log("to", temp);
-      //     setSendingMessages(temp);
-
-      //     break;
-      //   }
-      // }
-    });
+    localSocket.on("message", ({ doc, conversationId }) => addMessageToConversation(conversationId, doc, false));
+    localSocket.on("ack_message", ({ doc, conversationId }) => addMessageToConversation(conversationId, doc, true));
+    localSocket.on("start_typing", receiveStartTyping);
+    localSocket.on("stop_typing", receiveStopTyping);
   }, []);
 
   useEffect(() => {
-    if (socket === null || conversations?.length === 0 || !loading) return;
-    setLoading(false);
-  }, [socket, conversations]);
-
-  // changing selected chat
-  useEffect(() => {
-    console.log("Trying to change selected chat");
     if (selectedChatId !== null) {
       const found = conversations?.find((i) => i._id === selectedChatId);
-      console.log("found", found);
       if (found === undefined) return setSelectedChat(null);
       setSelectedChat(found);
-      s.current = found;
+      clearInterval(sendTypingInterval);
+      setSendTypingIntervalRunning(false);
+      setMessage("");
     }
   }, [selectedChatId, conversations]);
 
-  const handleSelectChat = async (id) => setSelectedChatId(id);
+  const addMessageToConversation = (conversationId, message, removeInSending) => {
+    const temp = [...conversationsRef.current];
+    for (let i = 0; i < temp.length; i++) {
+      if (temp[i]._id === conversationId) {
+        if (removeInSending) {
+          const index = temp[i].sending.findIndex((j) => j === message.message);
+          if (index !== -1) temp[i].sending.splice(index, 1);
+        }
+        temp[i].messages.push(message);
+        temp[i].last_message = { message: message.message, sender: message.sender, timestamp: message.timestamp };
+        temp.unshift(temp[i]);
+        temp.splice(i + 1, 1);
+        break;
+      }
+    }
+    setConversations(temp);
+    conversationsRef.current = temp;
+  };
 
-  useEffect(() => console.log("conversations:", conversations), [conversations]);
-  useEffect(() => console.log("selectedChat:", selectedChat), [selectedChat]);
-  useEffect(() => console.log("selectedChatId:", selectedChatId), [selectedChatId]);
+  const receiveStartTyping = ({ conversationId }) => {
+    const temp = [...conversationsRef.current];
+    for (let i = 0; i < temp.length; i++) {
+      if (temp[i]._id === conversationId) {
+        clearInterval(temp[i].intervalId);
+        temp[i].isTyping = true;
+        temp[i].intervalId = setInterval(() => {
+          temp[i].isTyping = false;
+          setConversations((prev) => {
+            const updated = [...prev];
+            updated[i].isTyping = false;
+            return updated;
+          });
+          clearInterval(temp[i].intervalId);
+        }, 11000);
+      }
+    }
+    conversationsRef.current = temp;
+    setConversations(temp);
+  };
+
+  const receiveStopTyping = ({ conversationId }) => {
+    const temp = [...conversationsRef.current];
+    for (let i = 0; i < temp.length; i++) {
+      if (temp[i]._id === conversationId) {
+        temp[i].isTyping = false;
+        clearInterval(sendTypingInterval);
+        break;
+      }
+    }
+    conversationsRef.current = temp;
+    setConversations(temp);
+  };
 
   const displayProfilePicture = (index) => {
     if (index + 1 >= selectedChat.messages.length) {
@@ -205,22 +111,19 @@ function Chat() {
     e.preventDefault();
     if (message === "") return;
     addSending();
+    socket.emit("stop_typing", { conversationId: selectedChatId });
+    clearInterval(sendTypingInterval);
+    setSendTypingIntervalRunning(false);
   };
 
   const addSending = () => {
-    // setConversations([]);
     const temp = [...conversationsRef.current];
-    console.log("old temp", temp);
     for (let i = 0; i < temp.length; i++) {
       if (temp[i]._id === selectedChatId) {
         temp[i].sending.push(message);
         temp.unshift(temp[i]);
         temp.splice(i + 1, 1);
-        console.log("new temp", temp);
-        console.log("sending: ", temp);
-        // conversationsRef.current = temp;
         break;
-        // return temp;
       }
     }
     conversationsRef.current = temp;
@@ -231,23 +134,19 @@ function Chat() {
     setMessage("");
   };
 
-  const addSendingTest = () => {
-    setConversations([
-      {
-        last_message: { sender: "Customer", message: "Is anyone here?", timestamp: "2023-08-20T00:50:00.000Z" },
-        last_message_date: "2023-08-20T00:50:00.000Z",
-        messages: [
-          { _id: "64e15da7dd1499e08e189381", sender: "Customer", message: "World", timestamp: "2023-08-20T00:26:15.353Z" },
-          { _id: "64e15dbcdd1499e08e189394", sender: "Customer", message: "Hello", timestamp: "2023-08-20T00:40:24.422Z" },
-          { _id: "64e15dbcdd1499e08e195391", sender: "Customer", message: "Is anyone here?", timestamp: "2023-08-20T00:50:00.000Z" },
-        ],
-        sending: [],
-        shop: { _id: "64d938ed01755ee3737c2e6e", name: "Jane Doe" },
-        status: "Sent",
-        _id: "64d9753bfb4f04acd5a354a8",
-      },
-    ]);
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+    if (e.target.value === "") {
+      socket.emit("stop_typing", { conversationId: selectedChatId });
+      clearInterval(sendTypingInterval);
+      setSendTypingIntervalRunning(false);
+    } else if (sendTypingIntervalRunning === false) {
+      sendTyping();
+      setSendingTypeInterval(setInterval(sendTyping, 5000));
+      setSendTypingIntervalRunning(true);
+    }
   };
+  const sendTyping = () => socket.emit("start_typing", { conversationId: selectedChatId });
 
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
@@ -255,8 +154,9 @@ function Chat() {
       <h3>Chat</h3>
       <div style={{ display: "flex", flexGrow: "1" }}>
         <div style={{ display: "flex", flexDirection: "column", width: "250px" }}>
+          {/* CONVERSATION TILES */}
           {conversations?.map((chat, index) => (
-            <div key={index} style={{ border: "1px solid black", padding: "0.5rem 1rem", cursor: "pointer" }} onClick={() => handleSelectChat(chat._id)}>
+            <div key={index} style={{ border: "1px solid black", padding: "0.5rem 1rem", cursor: "pointer" }} onClick={() => setSelectedChatId(chat._id)}>
               {userType === "Customer" && <div style={{ fontWeight: "500" }}>{chat.shop.name}</div>}
               {userType === "Shop" && <div style={{ fontWeight: "500" }}>{`${chat.customer.first_name} ${chat.customer.last_name}`}</div>}
               {userType !== chat.last_message?.sender && <div>{chat.last_message?.message}</div>}
@@ -274,8 +174,9 @@ function Chat() {
               </h5>
             </div>
 
-            {/* MESSAGES */}
+            {/* ALL MESSAGES */}
             <div style={{ display: "flex", flexDirection: "column", gap: "0.20rem", overflowY: "scroll", flexGrow: "1", height: "1px", padding: "1rem 0rem" }}>
+              {/* MESSAGES */}
               {selectedChat.messages.map((chat, index) => (
                 <div key={index}>
                   {chat?.sender === userType ? (
@@ -290,7 +191,6 @@ function Chat() {
                   )}
                 </div>
               ))}
-
               {/* SENDING MESSAGES */}
               {selectedChat.sending.length > 0 && (
                 <div style={{ display: "flex", flexDirection: "column", gap: "0.20rem", marginTop: "0.5rem" }}>
@@ -304,11 +204,13 @@ function Chat() {
                   </div>
                 </div>
               )}
+              {/* TYPING... */}
+              {selectedChat.isTyping && <div style={{ color: "#555555" }}>Typing...</div>}
             </div>
 
-            {/* TEXT BOX */}
+            {/* MESSAGE BOX */}
             <form style={{ display: "flex" }}>
-              <input type="text" placeholder="Enter a message" value={message} onChange={(e) => setMessage(e.target.value)} style={{ flex: 1 }} />
+              <input type="text" placeholder="Enter a message" value={message} onChange={handleMessageChange} style={{ flex: 1 }} />
               <button type="submit" onClick={handleSend}>
                 Send
               </button>
