1e369a835e480812916a7430469671812dc1eb37
j-sabile@gmail.com <jericosabile@gmail.com>
2023-09-01 02:06:39 +0800

    redesign homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Shops from "../components/Shops";
 
 function Home() {
+  const navigate = useNavigate();
   const [api, setApi] = useState("Loading...");
   const [userType, setUserType] = useState(null);
 
@@ -28,7 +29,21 @@ function Home() {
   return (
     <>
       <NavBar />
-      {api}
+      <section className="d-flex justify-content-center" style={{ minHeight: "100vh" }}>
+        <div className="container d-flex justify-content-between align-items-center">
+          <div className="d-flex flex-column justify-content-center align-items-start">
+            <p style={{ fontSize: "36pt", fontWeight: "600", color: "#000814" }}>Fresh Mangoes</p>
+            <button className="btn" style={{ backgroundColor: "#FFC300", color: "#F3F3F3", fontWeight: "500", padding: "0.5rem 2rem", fontSize: "16pt" }} onClick={() => navigate("/create-order")}>
+              Order Now
+            </button>
+          </div>
+          <div className="">
+            <img src="https://img.freepik.com/free-vector/yellow-mango-with-leaf-cartoon-sticker_1308-92449.jpg?w=2000" height="350" alt="mango" />
+          </div>
+        </div>
+      </section>
+
+      {/* {api}
       {userType === "Admin" && (
         <Link to="/admin">
           <button>Admin</button>
@@ -51,7 +66,7 @@ function Home() {
           <img src="https://img.freepik.com/free-vector/yellow-mango-with-leaf-cartoon-sticker_1308-92449.jpg?w=2000" height="150" alt="mango" />
         </div>
       </div>
-      <Shops />
+      <Shops /> */}
     </>
   );
 }
